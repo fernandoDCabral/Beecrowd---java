08114c0c58bffb2382d4426ecd9d3d6cd3f7e94b
fernandoDCabral <150301561+fernandoDCabral@users.noreply.github.com>
2024-07-09 13:27:38 -0300

    Update Beecrowd_1001.java

--- Beecrowd_1001.java
+++ Beecrowd_1001.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Beecrowd_1001 {
+public class Main {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         int A = Sc.nextInt();
