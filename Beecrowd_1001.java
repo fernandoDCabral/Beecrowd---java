9c027e3c552d51956ba46ca6e035199386f99df3
fernandoDCabral <150301561+fernandoDCabral@users.noreply.github.com>
2024-07-09 11:34:16 -0300

    Add files via upload

---
+++ Beecrowd_1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Beecrowd_1001 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int A = Sc.nextInt();
+        int B = Sc.nextInt();
+        System.out.println("X = "+(A+B));
+
+    }
+}
