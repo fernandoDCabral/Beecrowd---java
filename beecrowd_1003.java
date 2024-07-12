04041320a274d1e984b3d7ea6cfae716f114752f
Fernando Dutra <150301561+fernandoDCabral@users.noreply.github.com>
2024-07-12 16:26:50 -0300

    Add files via upload

---
+++ beecrowd_1003.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class beecrowd_1003 {
+    public static void main(String[] args) {
+
+        Scanner Sc = new Scanner(System.in);
+
+        int A = Sc.nextInt();
+
+        int B = Sc.nextInt();
+
+        System.out.println("SOMA = "+(A+B));
+
+    }
+}
